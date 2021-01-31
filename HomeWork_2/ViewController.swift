cba1f622fb3bcbf7a0e951d64ac5bd26bd61c02d
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-01-31 17:21:16 +0400

    Программно все работает. Остались констрэйнты.

--- HomeWork_2/ViewController.swift
+++ HomeWork_2/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ViewColor: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBAction func redSliderAction() {
+        changeColor()
+        redValueLabel.text = String(redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        changeColor()
+        greenValueLabel.text = String(greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        changeColor()
+        blueValueLabel.text = String(blueSlider.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func changeColor() {
+        ViewColor.backgroundColor = .init(displayP3Red: CGFloat(redSlider.value),
+                                          green: CGFloat(greenSlider.value),
+                                          blue: CGFloat(blueSlider.value),
+                                          alpha: CGFloat(1))
+    }
 
 }
 
